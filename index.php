aaf077b32af05e2163deddb31d15cbfcff5e95c8
h4nas <anosalhakimi123@gmail.com>
2025-09-28 15:38:43 +0300

    editing lines in indix.php file

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Online</title>
     <!-- Bootstrap CSS -->
+     
     <link href="bootstrap.min.css" rel="stylesheet">
     <style>
         body {
